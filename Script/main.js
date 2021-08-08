2788cf5b4cc6dbf0ead97fd4265c0e89fd4f1d5f
David Herrera <biggiegun@gmail.com>
2021-08-08 10:50:57 -0500

    Comentando código JS

--- Script/main.js
+++ Script/main.js
@@ -1,4 +1,6 @@
 // Code here!!
+
+// Declaración y asignación de variables !!
 const 
 screens = document.querySelectorAll('.screen'),
 
@@ -15,8 +17,15 @@ seconds = 0,
 score = 0,
 selected_insect = {}
 
+
+//  "Inicio del Juego", acá se "dispara" un evento al hacer click en "start_btn" 
 start_btn.addEventListener('click', ()=> screens[0].classList.add('up'))
 
+
+/* en "choose_insect_btns" tenemos guardado un array compuesto por elementos -insectos- a seleccionar del menú de juego!!
+la función forEach() acá descrita ejecuta, para el insecto seleccionado código que gracias a "setTimeout()", "createInsect()", y startGame(), establece tiempos y modos de generación de 
+insectos, e inicio del juego respectivamente
+*/
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', ()=>{
         const 
@@ -35,7 +44,8 @@ choose_insect_btns.forEach(btn => {
 function startGame(){
     setInterval(increaseTime, 1000)
 }
-    
+
+//  increaseTime() ejecuta un contador de tiempo de juego y lo "pinta en el DOM", haciendo uso de Template Literals. (En ediciones anteriores de la especificación ES2015, solían llamarse "plantillas de cadenas de caracteres").
 function increaseTime(){
     let 
     m = Math.floor(seconds / 60),
@@ -48,6 +58,7 @@ function increaseTime(){
     seconds++
 }
 
+// Como se mencionó createInsect() indica el modo en el cuál se crean los insectods, particularmente crea en pantalla un insecto dentro de un tag tipo "div" le añade los estilos de la clase 'insect' 
 function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
@@ -63,6 +74,7 @@ function createInsect() {
     game_container.appendChild(insect)
 }
 
+// El método acá descrito obtiene una ubicación aleatoria para el insecto
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
@@ -72,6 +84,7 @@ function getRandomLocation() {
     return{x,y}
 }
 
+// El método catchInsect() ejecuta el código para incrementar el puntaje a medida que los insectos son "removidos"/"Reescalados" a un tamaño "0" en pantalla.
 function catchInsect(){
     increaseScore()
     this.classList.add('caught')
@@ -84,6 +97,7 @@ function addInsects() {
     setTimeout(createInsect, 2500);
 }
 
+// La forma como aumenta el puntaje hasta que llega a "19" momento a partir del cual se muestra el mensaje: "Are you annoyed yet?  You are playing an impossible game !!"
 function increaseScore(){
     score++
     if(score >19){
